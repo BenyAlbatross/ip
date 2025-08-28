41ddb262ef09beef9ba4f4f22d45cd359809ab89
BenyAlbatross <chek.benjamin@gmail.com>
2025-08-29 00:46:19 +0800

    Add task storage and list command

--- src/main/java/Octoplush.java
+++ src/main/java/Octoplush.java
@@ -7,6 +7,9 @@ public class Octoplush {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int count = 0;
+
         // Greeting
         System.out.println(SEP);
         System.out.println(IND + "Hello! I'm Octoplush");
@@ -14,17 +17,29 @@ public class Octoplush {
         System.out.println(SEP);
 
         while (true) {
-            String line = sc.nextLine();
+            String line = sc.nextLine().trim();
+
             if (line.equals("bye")) {
-                // If user inputs "bye", exit
                 System.out.println(SEP);
                 System.out.println(IND + "Bye. Hope to see you again soon!");
                 System.out.println(SEP);
                 break;
-            } else {
-                // Echo user input
+            } else if (line.equals("list")) {
                 System.out.println(SEP);
-                System.out.println(IND + line);
+                for (int i = 0; i < count; i++) {
+                    System.out.println(IND + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(SEP);
+            } else if (!line.isEmpty()) {
+                // Add new task (assume <= 100)
+                tasks[count++] = line;
+                System.out.println(SEP);
+                System.out.println(IND + "added: " + line);
+                System.out.println(SEP);
+            } else {
+                // If input is empty, echo empty block
+                System.out.println(SEP);
+                System.out.println(IND);
                 System.out.println(SEP);
             }
         }
