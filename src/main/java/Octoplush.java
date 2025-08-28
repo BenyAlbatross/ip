10a79e31b1149a5f027bbf3b85096a40775fb158
BenyAlbatross <chek.benjamin@gmail.com>
2025-08-29 00:27:20 +0800

    Add input loop to echo commands and exit on bye
    The main class only printed the greeting and farewell messages without
    interacting with the user.
    
    Let's:
    * Add a loop to read user input
    * Echo back each command
    * Stop only when the user types "bye"
    This allows the chatbot to mimic a basic conversation flow.

--- src/main/java/Octoplush.java
+++ src/main/java/Octoplush.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+
 public class Octoplush {
+    private static final String SEP = "    ____________________________________________________________";
+    private static final String IND = "     ";
+
     public static void main(String[] args) {
-        String greeting = """
-            ____________________________________________________________
-            Hello! I'm Octoplush
-            What can I do for you?
-            """;
+        Scanner sc = new Scanner(System.in);
 
-        String farewell = """
-            ____________________________________________________________
-            Bye. Hope to see you again soon!
-            ____________________________________________________________
-            """;
+        // Greeting
+        System.out.println(SEP);
+        System.out.println(IND + "Hello! I'm Octoplush");
+        System.out.println(IND + "What can I do for you?");
+        System.out.println(SEP);
 
-        System.out.println(greeting);
-        System.out.println(farewell);
+        while (true) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                // If user inputs "bye", exit
+                System.out.println(SEP);
+                System.out.println(IND + "Bye. Hope to see you again soon!");
+                System.out.println(SEP);
+                break;
+            } else {
+                // Echo user input
+                System.out.println(SEP);
+                System.out.println(IND + line);
+                System.out.println(SEP);
+            }
+        }
+
+        sc.close();
     }
 }
