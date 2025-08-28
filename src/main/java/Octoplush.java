8a1a362caa482662302364e1d13617b99438bbcf
BenyAlbatross <chek.benjamin@gmail.com>
2025-08-29 00:12:04 +0800

    Change name of character from Duke to Octoplush

---
+++ src/main/java/Octoplush.java
@@ -0,0 +1,18 @@
+public class Octoplush {
+    public static void main(String[] args) {
+        String greeting = """
+            ____________________________________________________________
+            Hello! I'm Octoplush
+            What can I do for you?
+            """;
+
+        String farewell = """
+            ____________________________________________________________
+            Bye. Hope to see you again soon!
+            ____________________________________________________________
+            """;
+
+        System.out.println(greeting);
+        System.out.println(farewell);
+    }
+}
